5b8c8e3ad78a0b3cc776df421dcee705ae8d1df2
linh <thachlinhphan@gmail.com>
2022-08-13 15:24:36 +0200

    [ADD] db test

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
-
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Data.Text (Text)
+import Data.Functor.Identity
+import Database.CQL.IO as Client
+import qualified System.Logger as Logger
+main :: IO ([Identity Text])
+main =  do
+    
+    g <- Logger.new Logger.defSettings
+    c <- Client.init defSettings
+    let q = "SELECT cql_version from system.local" :: QueryString R () (Identity Text)
+    let p = defQueryParams One ()
+    runClient c (query q p)
